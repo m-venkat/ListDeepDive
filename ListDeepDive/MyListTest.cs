1eee99b6ee4ae38c3280a9b1d81dc43995357769
Venkatesh Muniyandi <venkatesh_muniyandi@dialog.com.au>
2018-12-04 06:53:58 +1100

    List implementation progress

--- ListDeepDive/MyListTest.cs
+++ ListDeepDive/MyListTest.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using MyCollections;
 using Xunit;
 
 namespace ListImplementationTest
 {
-    public class TestCSharpListFunctionality
+    public class MyListTest
     {
         private List<int> intList;
 
@@ -20,7 +19,7 @@ namespace ListImplementationTest
             public string City { get; set; }
         }
 
-        public TestCSharpListFunctionality()
+        public MyListTest()
         {
         }
 
@@ -39,7 +38,10 @@ namespace ListImplementationTest
         [Trait("XUnitTests", "C# List Tests")]
         public void AbleToInitializeCollection()
         {
-            intList = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            intList = new List<int>()
+            {
+                
+            };
             intList.Count().Should().Be(7, "It was intialized with 7 elements");
             String.Join(",", intList).Should().Be("1,2,3,4,5,6,7", "it should contain these elements");
         }
@@ -48,9 +50,22 @@ namespace ListImplementationTest
         [Trait("XUnitTests", "C# List Tests")]
         public void AbilityToQueryList()
         {
-            intList = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
-            var result = intList.Where(i => i >= 4);
-            String.Join(",", result).Should().Be("4,5,6,7", "it should contain these elements");
+            intList = new List<int>() { 1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13 };
+            //var result = intList.Where(i => i >= 4);
+            String.Join(",", intList).Should().Be("1,2,3,4,5,6,7,8,9,10,11,12,13", "it should contain these elements");
+        }
+
+
+        [Fact(DisplayName = "Count reflects exact and correct count")]
+        [Trait("XUnitTests", "C# List Tests")]
+        public void CountIsInSync()
+        {
+            intList = new List<int>();
+            for(int j = 1; j <= 300; j++) { 
+                intList.Add(j);
+                intList.Count().Should().Be(j);
+            }
+            intList.Count().Should().Be(300);
         }
 
 
@@ -80,39 +95,60 @@ namespace ListImplementationTest
                 new Employee(){Name = "Kishore",Age=54,Salary = 320,City ="Sydney"},
                 new Employee(){Name = "Vinod",Age=64,Salary = 420,City ="Adelaide"}
             };
-            empList.Count(item => item.City == "Melbourne").Should().Be(2);
+          
+            //empList.Count(item => item.City == "Melbourne").Should().Be(2);
         }
 
-        [Fact(DisplayName = "Ability to Add an item to collection thru Add Method")]
+        [Fact(DisplayName = "Ability to Remove the items from the List")]
         [Trait("XUnitTests", "C# List Tests")]
-        public void ListAddMethodWorks()
-        {
-            List<Employee> empList = new List<Employee>();
-            empList.Add(new Employee() { Name = "John", Age = 34, Salary = 120, City = "Melbourne" });
-            empList.Add(new Employee() { Name = "Ashok", Age = 44, Salary = 20, City = "Melbourne" });
-            empList.Count(item => item.City == "Melbourne").Should().Be(2);
-        }
 
-        [Fact(DisplayName = "Ability to call Remove Method and remove item")]
-        [Trait("XUnitTests", "C# List Tests")]
-        public void ListRemoveMethodWorks()
+        public void RemovalCountTest()
         {
-            List<Employee> empList = new List<Employee>();
-            Employee e1 = new Employee() {Name = "John", Age = 34, Salary = 120, City = "Melbourne"};
-            Employee e2 = new Employee() { Name = "Ashok", Age = 44, Salary = 20, City = "Melbourne" };
-            Employee e3 = new Employee() { Name = "John", Age = 34, Salary = 120, City = "Melbourne" };
-            empList.Add(e1);
-            empList.Add(e2);
-            empList.Count(item => item.City == "Melbourne").Should().Be(2);
-            empList.Remove(e2);
-            empList.Count(item => item.City == "Melbourne").Should().Be(1);
-            empList.Remove(e3);
-            empList.Count(item => item.City == "Melbourne").Should().Be(1);
-            var firstEmp = empList.FirstOrDefault();
-            empList.Remove(firstEmp);
-            empList.Count().Should().Be(0);
+           this.intList = new List<int>();
+            for (int j = 1; j <= 300; j++)
+            {
+                intList.Add(j);
+            }
+            intList.Count().Should().Be(300,"Added 300 so far");
+            for (int k = 0; k < 300; k++)
+            {
+                intList.Remove(k);
+                intList.Count().Should().Be(300 - (k + 1)," Inside loop");
+            }
 
         }
+
+
+        //[Fact(DisplayName = "Ability to Add an item to collection thru Add Method")]
+        //[Trait("XUnitTests", "C# List Tests")]
+        //public void ListAddMethodWorks()
+        //{
+        //    List<Employee> empList = new List<Employee>();
+        //    empList.Add(new Employee() { Name = "John", Age = 34, Salary = 120, City = "Melbourne" });
+        //    empList.Add(new Employee() { Name = "Ashok", Age = 44, Salary = 20, City = "Melbourne" });
+        //    empList.Count(item => item.City == "Melbourne").Should().Be(2);
+        //}
+
+        //[Fact(DisplayName = "Ability to call Remove Method and remove item")]
+        //[Trait("XUnitTests", "C# List Tests")]
+        //public void ListRemoveMethodWorks()
+        //{
+        //    List<Employee> empList = new List<Employee>();
+        //    Employee e1 = new Employee() {Name = "John", Age = 34, Salary = 120, City = "Melbourne"};
+        //    Employee e2 = new Employee() { Name = "Ashok", Age = 44, Salary = 20, City = "Melbourne" };
+        //    Employee e3 = new Employee() { Name = "John", Age = 34, Salary = 120, City = "Melbourne" };
+        //    empList.Add(e1);
+        //    empList.Add(e2);
+        //    empList.Count(item => item.City == "Melbourne").Should().Be(2);
+        //    empList.Remove(e2);
+        //    empList.Count(item => item.City == "Melbourne").Should().Be(1);
+        //    empList.Remove(e3);
+        //    empList.Count(item => item.City == "Melbourne").Should().Be(1);
+        //    var firstEmp = empList.FirstOrDefault();
+        //    empList.Remove(firstEmp);
+        //    empList.Count().Should().Be(0);
+
+        //}
 
 
         [Fact(DisplayName = "Able to iterate collection thru foreach")]
---
+++ Listt/List.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace MyCollections
+{
+    public class List<T> : IEnumerable<T>, IEnumerable
+    {
+        private T[] _items = new T[4];//Initial capacity 4
+        private int _size = 0;
+        public List()
+        {
+            
+        }
+
+        public T this[int index]
+        {
+          get
+          {
+              if ((uint) index >= (uint) this._items.Length)
+                  throw new ArgumentOutOfRangeException();
+              else
+                return this._items[index];
+            }
+            set
+            {
+                if ((uint)index >= (uint)this._items.Length)
+                    throw new ArgumentOutOfRangeException();
+                this._items[index] = value;
+            }
+        }
+
+        public int Count()
+        {
+            return _size;
+        }
+
+       
+
+        public void Add(T item)
+        {
+            if (_size >= _items.Length)
+            {
+                T[] destinArray = new T[_size*2];
+                Array.Copy(_items,destinArray,_size);
+                _items = destinArray;
+                _items[_size] = item;
+                _size++;
+            }
+            else
+            {
+                _items[_size] = item;
+                _size++;
+            }
+        }
+
+        public object Count(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T item)
+        {
+            int index = Array.IndexOf(_items, item, 0);
+            if (index > 0)
+            {
+                Array.Copy(_items, index+1, _items,index, _size-index);
+            }
+
+            this._size--;
+
+        }
+
+        public T FirstOrDefault()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i=0;i<=_size-1;i++)
+                 yield return _items[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
